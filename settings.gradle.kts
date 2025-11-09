1e1b8b3d3e40c9700b424c6fb723717c3a65942c
mdshahbaj-786 <mdshahbaj672@gmail.com>
2025-11-09 22:59:09 +0530

    Update plugin management with Android and Kotlin versions
    Added plugin versions for Android and Kotlin.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,16 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+    plugins {
+        id("com.android.application") version "8.6.0"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+        id("kotlin-kapt") version "1.9.24"
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories { google(); mavenCentral() }
 }
+
 rootProject.name = "TransportDocsManager"
 include(":app")
